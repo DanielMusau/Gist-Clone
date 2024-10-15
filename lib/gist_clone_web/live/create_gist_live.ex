584e19e84d126682c9c6b53911d51a109f935c20
DanielMusau <danielmusau.dm19@gmail.com>
2024-10-15 09:22:50 +0300

    added form display

---
+++ lib/gist_clone_web/live/create_gist_live.ex
@@ -0,0 +1,15 @@
+defmodule GistCloneWeb.CreateGistLive do
+  use GistCloneWeb, :live_view
+  import Phoenix.HTML.Form
+  alias GistClone.{Gists, Gists.Gist}
+
+  def mount(_params, _session, socket) do
+    socket =
+      assign(
+        socket,
+        form: to_form(Gists.change_gist(%Gist{}))
+      )
+
+    {:ok, socket}
+  end
+end
