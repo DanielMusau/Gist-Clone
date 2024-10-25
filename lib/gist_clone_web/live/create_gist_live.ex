a00c795b2060121a0a2f74d0bb794e3205d25686
DanielMusau <danielmusau.dm19@gmail.com>
2024-10-25 18:57:19 +0300

    Refactored the form component

--- lib/gist_clone_web/live/create_gist_live.ex
+++ lib/gist_clone_web/live/create_gist_live.ex
@@ -1,39 +1,10 @@
 defmodule GistCloneWeb.CreateGistLive do
   use GistCloneWeb, :live_view
-  import Phoenix.HTML.Form
 
+  alias GistCloneWeb.GistFormComponent
   alias GistClone.{Gists, Gists.Gist}
 
   def mount(_params, _session, socket) do
-    socket =
-      assign(
-        socket,
-        form: to_form(Gists.change_gist(%Gist{}))
-      )
-
     {:ok, socket}
-  end
-
-  def handle_event("validate", %{"gist" => params}, socket) do
-    changeset =
-      %Gist{}
-      |> Gists.change_gist(params)
-      |> Map.put(:action, :validate)
-
-    {:noreply, assign(socket, :form, to_form(changeset))}
-  end
-
-  def handle_event("create", %{"gist" => params}, socket) do
-    case Gists.create_gist(socket.assigns.current_user, params) do
-      {:ok, gist} ->
-        socket = push_event(socket, "clear-textarea", %{})
-        changeset = Gists.change_gist(%Gist{})
-        socket = assign(socket, form: to_form(changeset))
-        # Navigate to show the gist after successful creation
-        {:noreply, push_navigate(socket, to: ~p"/gist?#{[id: gist]}")}
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign(socket, :form, to_form(changeset))}
-    end
   end
 end
